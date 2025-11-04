2955a3210d1261d218f8a3c110d0270d259b6dc7
Sarveshwar <sarveshwar2810@gmail.com>
2025-11-05 00:19:29 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import Login from './pages/Login'
-import Signup from './pages/signup'
-import Home from './pages/home'
+import Signup from './pages/Signup'
+import Home from './pages/Home'
 import RefrshHandler from './RefreshHandler';
 
 
