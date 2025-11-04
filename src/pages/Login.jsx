b29f8271262843780dcfc9f3fb3149def082ea5b
Sarveshwar <sarveshwar2810@gmail.com>
2025-11-05 00:30:02 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -27,7 +27,7 @@ function Login() {
             return handleError('email and password are required')
         }
         try {
-            const url = `http://tic-tac-toe-backend-jade.vercel.app/auth/login`;
+            const url = `https://tic-tac-toe-backend-jade.vercel.app/auth/login`;
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
